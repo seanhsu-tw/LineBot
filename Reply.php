e4116b333de9739dac807cb99d7b4402e64a6383
seanhsu-tw <35246307+seanhsu-tw@users.noreply.github.com>
2018-01-09 06:21:06 +0000

    Reply

--- Reply.php
+++ Reply.php
@@ -23,7 +23,7 @@
 
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer JTPjyRwS8fmxHOQ/LgS9oORe3sfN9atdwZGxRsi4RovXOETa74p/OdEnZnwlv1AZgJnteGDtIvX7gcZHlAq1JTbgRaA1Tzu20lPKofgvLaQQmD4Ft4un4MBSEjCZjthOz77Ho7bitSKYRTpjGXGqjwdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
