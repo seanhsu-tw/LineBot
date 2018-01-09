d931f78a0936d2cb7163b5930fb7ea1a79023664
seanhsu-tw <35246307+seanhsu-tw@users.noreply.github.com>
2018-01-09 03:46:14 +0000

    save

--- receive.php
+++ receive.php
@@ -1,6 +1,7 @@
 <?php
-  $json_str=file_get_contexts('php://input');
-  $json_obj=json_decode($json_str);
+  $json_str = file_get_contexts('php://input');
+  $json_obj = json_decode($json_str);
+
   $my_file=fopen("log.txt","w+") or die("unable to open file!");
   fwrite($my_file,"\xEF\xBB\xBF".$json_str);
   fclose($my_file);
