96c00304dee0dd28a789fff2b33abb7ec8605f06
seanhsu-tw <35246307+seanhsu-tw@users.noreply.github.com>
2018-01-12 15:38:27 +0800

    Update Push.php

--- Push.php
+++ Push.php
@@ -14,10 +14,15 @@
 	switch ($sender_txt) {
 		case "upload":
 			$line_server_url = 'https://api.line.me/v2/bot/richmenu/$richmenuId/content';
-			$json_content = imagecreatefrompng("https://i.imgur.com/Ea5ZPDz.png");
-			$imagefile = fopen("controller.png", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
-			fwrite($imagefile, $json_content); 
-			fclose($imagefile);
+			
+			$m_img = imagecreatefrompng("https://i.imgur.com/Ea5ZPDz.png");
+			imagealphablending($m_img, false);
+			imagesavealpha($m_img, true);
+			imagepng($m_img, "controller.png", 0);
+
+			//$imagefile = fopen("controller.png", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
+			//fwrite($imagefile, $json_content); 
+			//fclose($imagefile);
 			
 			$response = array (
 				"controller.png"
